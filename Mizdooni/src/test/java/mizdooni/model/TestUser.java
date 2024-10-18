9813cfd92b9e7e8294ced40776a50f2871ae6263
matahho <mata1381@gmail.com>
2024-10-18 14:17:58 +0330

    add User models tests

---
+++ Mizdooni/src/test/java/mizdooni/model/TestUser.java
@@ -0,0 +1,77 @@
+package mizdooni.model;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestUser {
+    private User user;
+    private List<Reservation> reservations = new ArrayList<>();
+    private Restaurant restaurant;
+
+    @BeforeEach
+    public void setupFixtures(){
+
+        user = new User(
+                "Username",
+                "password@123",
+                "test@test.com",
+                Mockito.mock(Address.class),
+                User.Role.client
+        );
+        restaurant = Mockito.mock(Restaurant.class);
+        for (int i = 0 ; i < 10 ; i++)
+            reservations.add(new Reservation(user, restaurant, Mockito.mock(Table.class), LocalDateTime.now()));
+
+
+    }
+
+    @Test
+    public void newUserExists_tryToCheckHisPassword_hisPasswordCheckedCorrectly(){
+        String actualPassword = "password@123";
+        assertTrue(user.checkPassword(actualPassword), "Checking password method functionality");
+        assertFalse(user.checkPassword("Random-wrong-password"), "Checking password method functionality");
+    }
+
+    @Test
+    public void newUserExists_tryToAddReservations_reservationAddedCorrectly(){
+        for (Reservation reservation : reservations)
+            user.addReservation(reservation);
+        assertEquals(reservations.size(),user.getReservations().size());
+
+        for (Reservation reservation: user.getReservations())
+            assertEquals(9 , user.getReservation(9).getReservationNumber());
+    }
+
+    @Test
+    public void givenRestaurant_whenCheckIfReserved_thenCorrectReservationStatusReturned() {
+        Restaurant differentRestaurant = Mockito.mock(Restaurant.class);it a
+        Reservation reservation = new Reservation(user, restaurant, Mockito.mock(Table.class), LocalDateTime.now().minusDays(1));
+        Reservation futureReservation = new Reservation(user, restaurant, Mockito.mock(Table.class), LocalDateTime.now().plusDays(1));
+
+        user.addReservation(reservation);
+        user.addReservation(futureReservation);
+
+        assertTrue(user.checkReserved(restaurant), "The user should have a valid past reservation.");
+        assertFalse(user.checkReserved(differentRestaurant), "The user should not have a reservation at a different restaurant.");
+
+    }
+
+    @Test
+    public void reservationExists_userCanceledTheReservation_notCount(){
+        Reservation cancelledReservation = new Reservation(user, restaurant, Mockito.mock(Table.class), LocalDateTime.now().minusDays(1));
+        cancelledReservation.cancel();
+        user.addReservation(cancelledReservation);
+        assertFalse(user.checkReserved(restaurant));
+
+    }
+
+
+}
