6c3ffc2e025ae48901f3b91d99227947db3d61af
Alireza Hs <alireza.hosseini.81@gmail.com>
2024-11-01 23:25:14 +0330

    added remaining tests of ReservatonController

--- Mizdooni/src/test/java/mizdooni/controller/TestReservationController.java
+++ Mizdooni/src/test/java/mizdooni/controller/TestReservationController.java
@@ -2,6 +2,7 @@ package mizdooni.controller;
 
 import mizdooni.controllers.ControllerUtils;
 import mizdooni.controllers.ReservationController;
+import mizdooni.exceptions.ReservationCannotBeCancelled;
 import mizdooni.exceptions.UserNotManager;
 import mizdooni.model.*;
 import mizdooni.response.Response;
@@ -20,7 +21,9 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
@@ -59,8 +62,6 @@ public class TestReservationController {
 
     @Test
     public void reservationsExist_managerTryToGetCurrentReservations_retrievedCorrectly() throws Exception{
-        LocalDate date = LocalDate.of(2024, 10, 26);
-
         List<Reservation> mockReservations = List.of(new Reservation(client,restaurant,tables.get(0),LocalDateTime.now()));
         // I had to use eq() because of null date
         when(restaurantService.getRestaurant(restaurant.getId())).thenReturn(restaurant);
@@ -90,5 +91,66 @@ public class TestReservationController {
         verify(reserveService).getReservations(anyInt(), anyInt(), any(LocalDate.class));
     }
 
+    @Test
+    public void reservationTimesAvailable_userAsksForAvailableTimes_availableTimesShown() throws Exception{
+        when(restaurantService.getRestaurant(restaurant.getId())).thenReturn(restaurant);
+        when(reserveService.getAvailableTimes(anyInt(), anyInt(), any(LocalDate.class))).thenReturn(List.of());
+
+        Response response = reservationController.getAvailableTimes(restaurant.getId(), 1,LocalDate.now().toString());
+
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals("available times", response.getMessage());
+
+        verify(restaurantService).getRestaurant(restaurant.getId());
+        verify(reserveService).getAvailableTimes(anyInt(), eq(1), any(LocalDate.class));
+    }
+
+    @Test
+    public void noReservationsExist_userAddsReservation_reservationSuccessfullyAdded() throws Exception{
+        Reservation mockReservation = new Reservation(client, restaurant, tables.getFirst(), LocalDateTime.now());
+        Map<String, String> reservationParams = new HashMap<>();
+        reservationParams.put("people", "2");
+        reservationParams.put("datetime", "2024-04-08 12:30");
+
+        when(restaurantService.getRestaurant(restaurant.getId())).thenReturn(restaurant);
+        when(reserveService.reserveTable(anyInt(), anyInt(), any(LocalDateTime.class))).thenReturn(mockReservation);
+
+        Response response = reservationController.addReservation(restaurant.getId(), reservationParams);
+
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals("reservation done", response.getMessage());
+
+        verify(restaurantService).getRestaurant(restaurant.getId());
+        verify(reserveService).reserveTable(eq(restaurant.getId()), eq(2), any(LocalDateTime.class));
+    }
+
+    @Test
+    public void reservationsExist_userCancelsReservation_reservationSuccessfullyCancelled() throws Exception{
+        Reservation mockReservation = new Reservation(client, restaurant, tables.getFirst(), LocalDateTime.now().plusDays(1));
+        doAnswer(invocation -> {
+            mockReservation.cancel();
+            return null;
+        }).when(reserveService).cancelReservation(mockReservation.getReservationNumber());
+        Response response = reservationController.cancelReservation(mockReservation.getReservationNumber());
+
+
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertTrue(mockReservation.isCancelled());
+
+        verify(reserveService).cancelReservation(mockReservation.getReservationNumber());
+    }
+
+    @Test
+    public void reservationsExist_userCancelsReservationAfterReservedTime_fails() throws Exception{
+        Reservation mockReservation = new Reservation(client, restaurant, tables.getFirst(), LocalDateTime.now().minusDays(1));
+        doThrow(new ReservationCannotBeCancelled()).when(reserveService).cancelReservation(mockReservation.getReservationNumber());
+
+        ResponseException exception = assertThrows(ResponseException.class, ()->reservationController.cancelReservation(mockReservation.getReservationNumber()));
+
+        assertEquals(HttpStatus.BAD_REQUEST,exception.getStatus());
+        assertEquals("ReservationCannotBeCancelled", exception.getError());
+
+        verify(reserveService).cancelReservation(mockReservation.getReservationNumber());
+    }
 
 }
