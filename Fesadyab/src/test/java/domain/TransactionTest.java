1ff80b19e0b47a9e21de577cd2e60049fa5e302d
matahho <mata1381@gmail.com>
2024-11-15 17:02:00 +0330

    100% coverage for the Transaction class

--- Fesadyab/src/test/java/domain/TransactionTest.java
+++ Fesadyab/src/test/java/domain/TransactionTest.java
@@ -1,4 +1,41 @@
 package domain;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class TransactionTest {
+    Transaction t1 = new Transaction();
+    Transaction t2 = new Transaction();
+    @BeforeEach
+    public void set_up(){
+        // Set up T1
+        t1.transactionId = 1;
+        t1.accountId = 1;
+        t1.amount = 100;
+        t1.isDebit = false;
+
+        // Set up T2
+        t2.transactionId = 2;
+        t2.accountId = 2;
+        t2.amount = 200;
+        t2.isDebit = false;
+
+    }
+    @Test
+    public void twoTransactionExistsWithDifferentID_checkIfTheyAreEqual_returnedNotEqual(){
+        assertFalse(t1.equals(t2));
+    }
+    @Test
+    public void twoTransactionExistsWithSameID_checkIfTheyAreEqual_returnedEqual(){
+        t2.transactionId = t1.transactionId;
+        assertTrue(t1.equals(t2));
+    }
+    @Test
+    public void oneTransactionExists_checkIfItsEqualWithAnotherOBJ_returnedNotEqual(){
+        assertFalse(t1.equals(new String("Mahdi")));
+    }
+
 }
