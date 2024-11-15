a68f532eeec1f6efb12bfc6ca7f5b1f564561962
Alireza Hs <alireza.hosseini.81@gmail.com>
2024-11-15 23:35:10 +0330

    added tests for detection and patternThreshold

--- Fesadyab/src/test/java/domain/TransactionEngineTest.java
+++ Fesadyab/src/test/java/domain/TransactionEngineTest.java
@@ -3,7 +3,7 @@ package domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TransactionEngineTest {
 
@@ -16,7 +16,7 @@ public class TransactionEngineTest {
 
     //getAverageTransactionAmountByAccount Tests
     @Test
-    void noTransaction_getAverageTransactionAmountByAccount_zeroReturned() {
+    public void noTransaction_getAverageTransactionAmountByAccount_zeroReturned() {
         assertEquals(0, engine.getAverageTransactionAmountByAccount(1));
     }
 
@@ -40,12 +40,54 @@ public class TransactionEngineTest {
     }
 
     @Test
-    public void transactionExist_NewTransactionAdded_Fraudulent() {
+    public void transactionExist_NewTransactionAdded_nonFraudulent() {
         engine.transactionHistory.add(new Transaction(1, 1, 100, false));
         engine.transactionHistory.add(new Transaction(2, 1, 200, false));
 
         Transaction txn = new Transaction(3, 1, 450, false);
         assertEquals(0, engine.addTransactionAndDetectFraud(txn));
         assertEquals(3, engine.transactionHistory.size());
+    }
+
+    @Test
+    public void transactionExist_NewTransactionAdded_Fraudulent() {
+        engine.transactionHistory.add(new Transaction(1, 1, 100, false));
+        engine.transactionHistory.add(new Transaction(2, 1, 200, false));
+
+        Transaction txn = new Transaction(3, 1, 450, true);
+        assertNotEquals(0, engine.addTransactionAndDetectFraud(txn));
+        assertEquals(3, engine.transactionHistory.size());
+    }
+
+    @Test
+    public void noTransaction_getTransactionPatternAboveThreshold_zeroReturned() {
+        assertEquals(0, engine.getTransactionPatternAboveThreshold(1));
+    }
+
+    @Test
+    public void transactionsWithEqualGapsExist_getTransactionPatternWithThreshold_correctDifferenceReturned(){
+        engine.transactionHistory.add(new Transaction(1, 1, 100, false));
+        engine.transactionHistory.add(new Transaction(2, 1, 200, false));
+        engine.transactionHistory.add(new Transaction(3, 2, 300, false));
+
+        assertEquals(100, engine.getTransactionPatternAboveThreshold(50));
+    }
+
+    @Test
+    public void transactionsWithDifferentGapsExist_getTransactionPatternWithThreshold_correctDifferenceReturned(){
+        engine.transactionHistory.add(new Transaction(1, 1, 100, false));
+        engine.transactionHistory.add(new Transaction(2, 1, 200, false));
+        engine.transactionHistory.add(new Transaction(3, 2, 400, false));
+
+        assertEquals(0, engine.getTransactionPatternAboveThreshold(101));
+    }
+
+    @Test
+    public void transactionsExists_newTemporaryTransactionComes_newTransactionDetectedAsSuspicious(){
+        engine.transactionHistory.add(new Transaction(1, 1, 100, false));
+        engine.transactionHistory.add(new Transaction(2, 1, 200, false));
+        engine.transactionHistory.add(new Transaction(3, 1, 300, false));
+
+        assertNotEquals(0, engine.detectFraudulentTransaction(new Transaction(4, 1, 500, true)));
     }
 }
