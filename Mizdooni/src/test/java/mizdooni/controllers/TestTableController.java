01159da92865e9173ec12c041c85fe1d263a5510
Alireza Hs <alireza.hosseini.81@gmail.com>
2024-11-28 19:26:24 +0330

    complete TableControllerTest, fixed a typo in TableController

--- Mizdooni/src/test/java/mizdooni/controllers/TestTableController.java
+++ Mizdooni/src/test/java/mizdooni/controllers/TestTableController.java
@@ -1,12 +1,16 @@
 package mizdooni.controllers;
 
 import mizdooni.exceptions.RestaurantNotFound;
+import mizdooni.model.Address;
 import mizdooni.model.Restaurant;
 import mizdooni.model.Table;
+import mizdooni.model.User;
 import mizdooni.service.RestaurantService;
 import mizdooni.service.TableService;
 import mizdooni.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,12 +19,14 @@ import org.springframework.test.web.servlet.MvcResult;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import java.time.LocalTime;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyInt;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -69,6 +75,58 @@ public class TestTableController {
                 .andExpect(jsonPath("$.error").value("RestaurantNotFound"))
                 .andExpect(jsonPath("$.message").value("Restaurant not found.")); //Complete this call
 
+    }
+
+    @Test
+    void aRestaurantExists_addTableWithValidInput_ReturnSuccessResponse() throws Exception {
+        String requestBody = "{\"seatsNumber\":\"4\"}";
+        User manager = new User("Manager1", "TestManagerPassword", "manager@example.com", new Address("TestCountry", "TestCity", null), User.Role.manager);
+        Restaurant r1 = new Restaurant("testRestaurant-1", manager,"TestType1", LocalTime.of(12,0),LocalTime.of(23,0),"test Descriptions1", Mockito.mock(Address.class), "Test Image1");
+
+        when(restaurantService.getRestaurant(anyInt())).thenReturn(r1);
+
+        // Mocking addTable to not throw any exception
+        doNothing().when(tableService).addTable(anyInt(), anyInt());
+
+        this.mockMvc.perform(post("/tables/{restaurantId}", r1.getId())
+                        .contentType("application/json")
+                        .content(requestBody))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.success").value(true))
+                .andExpect(jsonPath("$.message").value("table added"))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty());
+    }
+
+    @Test
+    void aRestaurantExists_addTableWithMissingParams_ReturnBadRequestParamsMissing() throws Exception {
+        String requestBody = "{\"wrongKey\":\"4\"}";
+
+        when(restaurantService.getRestaurant(anyInt())).thenReturn(mock(Restaurant.class));
+
+        this.mockMvc.perform(post("/tables/{restaurantId}", 1)
+                        .contentType("application/json")
+                        .content(requestBody))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.message").value("parameters missing"))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty());
+    }
+
+    @Test
+    void aRestaurantExists_addTableWithWrongTypeParams_ReturnBadRequestParamsBadType() throws Exception {
+        String requestBody = "{\"seatsNumber\":\"test\"}";
+
+        when(restaurantService.getRestaurant(anyInt())).thenReturn(mock(Restaurant.class));
+
+        this.mockMvc.perform(post("/tables/{restaurantId}", 1)
+                        .contentType("application/json")
+                        .content(requestBody))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success").value(false))
+                .andExpect(jsonPath("$.status").value(400))
+                .andExpect(jsonPath("$.message").value("bad parameter type"))
+                .andExpect(jsonPath("$.timestamp").isNotEmpty());
     }
 
 }
